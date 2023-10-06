6aab42b2563e2973f99729a11ca29d92a5dd5780
Julio <julio958214@gmail.com>
2023-10-06 16:15:03 -0300

    refactory

--- src/test/java/com/bookstore/specs/Login.java
+++ src/test/java/com/bookstore/specs/Login.java
@@ -7,7 +7,7 @@ import org.json.simple.JSONObject;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
-import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 
 import static org.hamcrest.Matchers.*;
@@ -21,80 +21,81 @@ import java.util.Map;
 
 @Tag("regression")
 public class Login extends BaseApi {
-        public static final Map<String, String> payload = new HashMap<>();
-        public static JSONObject payloadJson;
+    public static final Map<String, Object> payload = new HashMap<>();
+    public static JSONObject payloadJson;
 
-        @Test
-        @Description("Login sucessfully return - 200")
-        public void login_sucessfully() {
-                payload.put("userName", "QA_2");
-                payload.put("password", "Test@123");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @Tag("login")
+    @DisplayName("Login sucessfully return - 200")
+    public void loginSucessfully() {
+        payload.put("userName", "QA_2");
+        payload.put("password", "Test@123");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/GenerateToken")
-                        .then()
-                                .statusCode(HttpStatus.SC_OK)
-                        .body(
-                                "token", is(notNullValue()),
-                                "expires", is(notNullValue()),
-                                "status", is("Success"),
-                                "result", is("User authorized successfully."));   
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(
+                        "token", is(notNullValue()),
+                        "expires", is(notNullValue()),
+                        "status", is("Success"),
+                        "result", is("User authorized successfully."));
+    }
 
-        @Test
-        @Description("Login with invalid username return - 400")
-        public void login_with_invalid_username() {
-                payload.put("userName", "invalid_userName");
-                payload.put("password", "Test@123");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Login with invalid username return - 400")
+    public void loginWithInvalidUsername() {
+        payload.put("userName", "invalid_userName");
+        payload.put("password", "Test@123");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/GenerateToken")
-                        .then().body(
-                                "status", is("Failed"),
-                                "result", is("User authorization failed."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then().body(
+                        "status", is("Failed"),
+                        "result", is("User authorization failed."));
+    }
 
-        @Test
-        @Description("Login with invalid password return - 400")
-        public void login_with_invalid_password() {
-                payload.put("userName", "QA_2");
-                payload.put("password", "invalid_password");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Login with invalid password return - 400")
+    public void loginWithInvalidPassword() {
+        payload.put("userName", "QA_2");
+        payload.put("password", "invalid_password");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/GenerateToken")
-                        .then()
-                                .body(
-                                        "status", is("Failed"),
-                                        "result", is("User authorization failed."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .body(
+                        "status", is("Failed"),
+                        "result", is("User authorization failed."));
+    }
 
-        @Test
-        @Description("Login with invalid username and password  return - 400")
-        public void login_with_invalid_username_and_password () {
-                payload.put("userName", "invalid_userName");
-                payload.put("password", "invalid_password");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Login with invalid username and password  return - 400")
+    public void loginWithInvalidUsernameAndPassword() {
+        payload.put("userName", "invalid_userName");
+        payload.put("password", "invalid_password");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/GenerateToken")
-                        .then()
-                                .body(
-                                        "status", is("Failed"),
-                                        "result", is("User authorization failed."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .body(
+                        "status", is("Failed"),
+                        "result", is("User authorization failed."));
+    }
 }
--- src/test/java/com/bookstore/specs/RegistUser.java
+++ src/test/java/com/bookstore/specs/RegistUser.java
@@ -9,7 +9,8 @@ import com.github.javafaker.Faker;
 import com.bookstore.support.BaseApi;
 
 import io.restassured.RestAssured;
-import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 
 import static org.hamcrest.Matchers.*;
 
@@ -20,131 +21,132 @@ import java.util.Map;
  * @author Julio C. Santos
  */
 
+@Tag("regression")
 public class RegistUser extends BaseApi {
-        public static final Map<String, String> payload = new HashMap<>();
-        public static JSONObject payloadJson;
-        private static final Faker faker = new Faker();
-        private static final String userName = faker.address().firstName();
+    public static final Map<String, Object> payload = new HashMap<>();
+    public static JSONObject payloadJson;
+    private static final Faker faker = new Faker();
+    private static final String userName = faker.address().firstName();
 
-        @Test
-        @Description("New account registration return - 201")
-        public void new_account_registration() {
-                payload.put("userName", userName);
-                payload.put("password", "Mudar@123");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Registration of a new account return 201")
+    public void newAccountRegistration() {
+        payload.put("userName", userName);
+        payload.put("password", "Mudar@123");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_CREATED)
-                        .body(
-                                "userID", is(notNullValue()),
-                                "username", is(notNullValue()),
-                                "books", is(notNullValue()),
-                                "username", is(userName),
-                                "books", empty());
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body(
+                        "userID", is(notNullValue()),
+                        "username", is(notNullValue()),
+                        "books", is(notNullValue()),
+                        "username", is(userName),
+                        "books", empty());
+    }
 
-        @Test
-        @Description("Create an account with blank userName return 400")
-        public void create_an_account_with_blank_userName() {
-                payload.put("userName", "");
-                payload.put("password", "Mudar@123");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Create an account with blank userName return 400")
+    public void createAnAccountWithBlankUserName() {
+        payload.put("userName", "");
+        payload.put("password", "Mudar@123");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                        .body(
-                                "code", is("1200"),
-                                "message", is("UserName and Password required."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(
+                        "code", is("1200"),
+                        "message", is("UserName and Password required."));
+    }
 
-        @Test
-        @Description("Create an account with blank password return 400")
-        public void create_an_account_with_blank_password() {
-                payload.put("userName", userName);
-                payload.put("password", "");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Create an account with blank password return 400")
+    public void createAnAccountWithBlankPassword() {
+        payload.put("userName", userName);
+        payload.put("password", "");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                        .body(
-                                "code", is("1200"),
-                                "message", is("UserName and Password required."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(
+                        "code", is("1200"),
+                        "message", is("UserName and Password required."));
+    }
 
-        @Test
-        @Description("Create an account with blank username and password")
-        public void create_an_account_with_blank_username_and_password() {
-                payload.put("userName", "");
-                payload.put("password", "");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Create an account with blank username and password")
+    public void createAnAccountWithBlankUsernameAndPassword() {
+        payload.put("userName", "");
+        payload.put("password", "");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                        .body(
-                                "code", is("1200"),
-                                "message", is("UserName and Password required."));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(
+                        "code", is("1200"),
+                        "message", is("UserName and Password required."));
+    }
 
-        @Test
-        @Description("Create an account with a password that does not contain special characters return 400")
-        public void create_an_account_with_a_password_that_does_not_contain_special_characters() {
-                payload.put("userName", userName);
-                payload.put("password", "passwd");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Create an account with a password that does not contain special characters return 400")
+    public void createAnAccountWithaPasswordThatDoesNotContainSpecialCharacters() {
+        payload.put("userName", userName);
+        payload.put("password", "passwd");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                        .body(
-                                "code", is("1300"),
-                                "message", is(
-                                        "Passwords must have at least one non alphanumeric character, " +
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(
+                        "code", is("1300"),
+                        "message", is(
+                                "Passwords must have at least one non alphanumeric character, " +
                                         "one digit (\'0\'-\'9\'), one uppercase (\'A\'-\'Z\'), " +
                                         "one lowercase (\'a\'-\'z\'), one special character and " +
                                         "Password must be eight characters or longer."));
-        }
+    }
 
-        @Test
-        @Description("Create an account with the same data as an existing account return 400")
-        public void create_an_account_with_the_same_data_as_an_existing_account() {
-                payload.put("userName", "user1");
-                payload.put("password", "Mudar@123");
-                payloadJson = new JSONObject(payload);
+    @Test
+    @DisplayName("Create an account with the same data as an existing account return 400")
+    public void createAnAccountWithTheSameDataAsAnExistingAccount() {
+        payload.put("userName", "user1");
+        payload.put("password", "Mudar@123");
+        payloadJson = new JSONObject(payload);
 
-                RestAssured
-                        .given()
-                                .body(payloadJson.toJSONString())
-                        .when()
-                                .post("/Account/v1/User")
-                        .then()
-                                .statusCode(HttpStatus.SC_NOT_ACCEPTABLE)
-                        .body(
-                                "code", is("1204"),
-                                "message", is("User exists!"));
-        }
+        RestAssured
+                .given()
+                .body(payloadJson.toJSONString())
+                .when()
+                .post("/Account/v1/User")
+                .then()
+                .statusCode(HttpStatus.SC_NOT_ACCEPTABLE)
+                .body(
+                        "code", is("1204"),
+                        "message", is("User exists!"));
+    }
 
 }
