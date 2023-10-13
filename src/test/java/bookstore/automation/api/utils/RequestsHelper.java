e7de0d87b91bb357d2798b1bae0e912b4fd297f6
Julio Santos <julioc.9730@gmail.com>
2023-10-13 01:07:14 -0300

    Atualizar o RequestsHelper.java

--- src/test/java/bookstore/automation/api/utils/RequestsHelper.java
+++ src/test/java/bookstore/automation/api/utils/RequestsHelper.java
@@ -8,26 +8,26 @@ public class RequestsHelper {
 
     public static String getTokenLoginUer(String userName, String Password) {
         LoginDmn user = new LoginDmn(userName, Password);
-        return RestAssured.
-                given().
-                body(user).
-                when().
-                post("/GenerateToken").
-                then().
-                statusCode(HttpStatus.SC_OK).
-                extract().path("token").toString();
+        return RestAssured
+                .given()
+                .body(user)
+                .when()
+                .post("/GenerateToken")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().path("token").toString();
     }
 
     public static String getUserId(String userName, String Password) {
         LoginDmn user = new LoginDmn(userName, Password);
-        return RestAssured.
-                given().
-                body(user).
-                when().
-                post("/User").
-                then().
-                statusCode(HttpStatus.SC_CREATED).
-                extract().path("userID").toString();
+        return RestAssured
+                .given()
+                .body(user)
+                .when()
+                .post("/User")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED).
+                .extract().path("userID").toString();
     }
 
 }
